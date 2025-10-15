9422cf3844416130fbb717f1859932f573c1e214
Atharvg1289 <gaikwadjayshri83@gmail.com>
2025-10-15 15:52:53 +0530

    Update StatesSection.tsx

--- components/StatesSection.tsx
+++ components/StatesSection.tsx
@@ -43,7 +43,7 @@ const stats = [
 ];
 
 
-const StatsSection: React.FC = () => {
+export const StatsSection: React.FC = () => {
     return (
         <AnimatedSection className="py-20 bg-white">
             <div className="container mx-auto px-4 sm:px-6 lg:px-8">
@@ -66,5 +66,3 @@ const StatsSection: React.FC = () => {
         </AnimatedSection>
     )
 }
-
-export default StatsSection;
