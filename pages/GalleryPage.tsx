b40938dba4df08189320692a327b5bbc2b5933e5
Atharvg1289 <gaikwadjayshri83@gmail.com>
2025-10-15 15:51:36 +0530

    Update GalleryPage.tsx

--- pages/GalleryPage.tsx
+++ pages/GalleryPage.tsx
@@ -7,7 +7,7 @@ const galleryImages = [
   { src: '/images/1.jpg', alt: 'Rooftop solar panel installation on a residential building' },
   { src: '/images/2.jpg', alt: 'Solar panels installed on an industrial rooftop' },
   { src: '/images/3.jpg', alt: 'Detailed view of a solar panel array' },
-  { src: '/images/4.jpg', alt: 'Green Turn Solar team completed another installation' },
+  { src: '/images/4.jpg', alt: 'Green Turn Solar team inspecting a completed installation' },
 ];
 
 const GalleryPage: React.FC = () => {
@@ -23,7 +23,8 @@ const GalleryPage: React.FC = () => {
               <ImageWithSpinner 
                 src={image.src} 
                 alt={image.alt} 
-                imageClassName="transition-transform duration-300 group-hover:scale-110" 
+                imageClassName="transition-transform duration-300 group-hover:scale-110"
+                containerClassName="bg-gray-100"
               />
               <div className="absolute inset-0 bg-black bg-opacity-0 group-hover:bg-opacity-50 transition-all duration-300 flex items-end p-4">
                 <p className="text-white text-sm opacity-0 group-hover:opacity-100 transition-opacity duration-300 translate-y-4 group-hover:translate-y-0">
