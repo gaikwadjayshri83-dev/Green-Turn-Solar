2f5bb8a91ecfd25046b553a59c2a3afc72599032
Atharvg1289 <gaikwadjayshri83@gmail.com>
2025-10-27 18:35:39 +0530

    Update TestimonialsPage.tsx

--- pages/TestimonialsPage.tsx
+++ pages/TestimonialsPage.tsx
@@ -2,16 +2,22 @@ import React from 'react';
 import PageWrapper from '../components/PageWrapper';
 import Testimonials from '../components/Testimonials';
 import AnimatedSection from '../components/common/AnimatedSection';
+import GoogleReviews from '../components/GoogleReviews';
 
 const TestimonialsPage: React.FC = () => {
   return (
     <PageWrapper
       title="What Our Customers Say"
-      subtitle="We are proud to have powered homes and businesses across Nagpur with reliable solar energy."
+      subtitle="We are proud to have powered homes and businesses across Nagpur with reliable solar energy. See our verified reviews on Google."
       className="bg-green-50"
     >
+      <AnimatedSection className="mb-16">
+        <GoogleReviews />
+      </AnimatedSection>
       <AnimatedSection>
-        <Testimonials showHeading={false} />
+        <div className="bg-white py-16">
+          <Testimonials showHeading={false} />
+        </div>
       </AnimatedSection>
     </PageWrapper>
   );
